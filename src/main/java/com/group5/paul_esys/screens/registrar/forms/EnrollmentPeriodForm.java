4c049650975f62950714208e7507bb0850a9dbc8
Vince Angelo Batecan <llTheBlankll@gmail.com>
2026-04-10 11:24:46 +0800

    feat: refactor EnrollmentPeriodForm to extend JFrame, update layout and event handling

--- src/main/java/com/group5/paul_esys/screens/registrar/forms/EnrollmentPeriodForm.java
+++ src/main/java/com/group5/paul_esys/screens/registrar/forms/EnrollmentPeriodForm.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package com.group5.paul_esys.screens.registrar.forms;
 
@@ -8,10 +8,12 @@ package com.group5.paul_esys.screens.registrar.forms;
  *
  * @author nytri
  */
-public class EnrollmentPeriodForm extends javax.swing.JPanel {
+public class EnrollmentPeriodForm extends javax.swing.JFrame {
+	
+	private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(EnrollmentPeriodForm.class.getName());
 
 	/**
-	 * Creates new form EnrollmentPeriodForm
+	 * Creates new form EnrollmentPeriodF
 	 */
 	public EnrollmentPeriodForm() {
 		initComponents();
@@ -44,10 +46,11 @@ public class EnrollmentPeriodForm extends javax.swing.JPanel {
                 btnSave = new javax.swing.JButton();
                 btnCanel = new javax.swing.JButton();
 
-                setLayout(new javax.swing.BoxLayout(this, javax.swing.BoxLayout.Y_AXIS));
+                setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+                getContentPane().setLayout(new javax.swing.BoxLayout(getContentPane(), javax.swing.BoxLayout.Y_AXIS));
 
                 windowBar1.setTitle("Enrollment Period Form");
-                add(windowBar1);
+                getContentPane().add(windowBar1);
 
                 jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
@@ -81,10 +84,12 @@ public class EnrollmentPeriodForm extends javax.swing.JPanel {
                 btnSave.setBackground(new java.awt.Color(119, 0, 0));
                 btnSave.setForeground(new java.awt.Color(255, 255, 255));
                 btnSave.setText("Save");
+                btnSave.addActionListener(this::btnSaveActionPerformed);
 
                 btnCanel.setBackground(new java.awt.Color(119, 0, 0));
                 btnCanel.setForeground(new java.awt.Color(255, 255, 255));
                 btnCanel.setText("Cancel");
+                btnCanel.addActionListener(this::btnCanelActionPerformed);
 
                 javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
                 jPanel1.setLayout(jPanel1Layout);
@@ -94,13 +99,13 @@ public class EnrollmentPeriodForm extends javax.swing.JPanel {
                                 .addContainerGap()
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                         .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 338, Short.MAX_VALUE))
+                                        .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                                 .addContainerGap())
                         .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGap(54, 54, 54)
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                         .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addComponent(txtSemester, javax.swing.GroupLayout.DEFAULT_SIZE, 243, Short.MAX_VALUE)
+                                        .addComponent(txtSemester)
                                         .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                         .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                         .addComponent(txtSchoolYear)
@@ -108,8 +113,8 @@ public class EnrollmentPeriodForm extends javax.swing.JPanel {
                                         .addComponent(spinnerEndDate)
                                         .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                         .addComponent(jLabel7, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addComponent(jScrollPane1))
-                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 243, Short.MAX_VALUE))
+                                .addContainerGap(51, Short.MAX_VALUE))
                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(btnCanel)
@@ -120,11 +125,11 @@ public class EnrollmentPeriodForm extends javax.swing.JPanel {
                 jPanel1Layout.setVerticalGroup(
                         jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addContainerGap()
+                                .addGap(29, 29, 29)
                                 .addComponent(jLabel1)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(jLabel2)
-                                .addGap(54, 54, 54)
+                                .addGap(31, 31, 31)
                                 .addComponent(jLabel3)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(txtSchoolYear, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -148,12 +153,46 @@ public class EnrollmentPeriodForm extends javax.swing.JPanel {
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                                         .addComponent(btnSave)
                                         .addComponent(btnCanel))
-                                .addContainerGap(49, Short.MAX_VALUE))
+                                .addContainerGap(48, Short.MAX_VALUE))
                 );
 
-                add(jPanel1);
+                getContentPane().add(jPanel1);
+
+                pack();
         }// </editor-fold>//GEN-END:initComponents
 
+        private void btnSaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSaveActionPerformed
+                // TODO add your handling code here:
+        }//GEN-LAST:event_btnSaveActionPerformed
+
+        private void btnCanelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCanelActionPerformed
+		dispose();
+        }//GEN-LAST:event_btnCanelActionPerformed
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String args[]) {
+		/* Set the Nimbus look and feel */
+		//<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+		/* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+		 */
+		try {
+			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
+			logger.log(java.util.logging.Level.SEVERE, null, ex);
+		}
+		//</editor-fold>
+
+		/* Create and display the form */
+		java.awt.EventQueue.invokeLater(() -> new EnrollmentPeriodForm().setVisible(true));
+	}
 
         // Variables declaration - do not modify//GEN-BEGIN:variables
         private javax.swing.JButton btnCanel;
